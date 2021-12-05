ae20d96212eaa593775123cd66c8c28f4d383fe1
William Claster <william108@gmail.com>
2021-12-05 20:28:04 +0900

    #I fixed the conflict by deleting the local initiative. Now it says I am up to date. I will now try to add this change.

--- R/testError.R
+++ R/testError.R
@@ -6,3 +6,4 @@
 #add another 2
 #add remote again
 #this change will be added remotely and then I will try to push a different change from local
+#I fixed the conflict by deleting the local initiative. Now it says I am up to date. I will now try to add this change.
